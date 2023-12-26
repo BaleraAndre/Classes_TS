55ed93b77cb7e1341901eb14dfacfc3e3737278a
baleraandre <andremarinobalera1@gmail.com>
2023-12-26 14:12:10 -0300

    CRUD TS

--- index.ts
+++ index.ts
@@ -1,63 +1,77 @@
-interface Pessoa<T> {
-    nome: string;
-    idade: T;
-    endereco: {
-      rua: string;
-      cidade: string;
-    };
-  }
-  
-  class Gerenciador<T> {
-    private itens: T[] = [];
-  
-    adicionarItem(item: T): void {
-      this.itens.push(item);
-    }
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { Pool } from 'pg';
 
-    removerItem(condicao: (item: T) => boolean): void {
-      this.itens = this.itens.filter((item) => !condicao(item));
-    }
-  
-    obterItens(): T[] {
-      return this.itens;
-    }
-  
-    calcularMedia<A extends number>(getValorNumerico: (item: T) => A): number {
-      if (this.itens.length === 0) {
-        return 0;
-      }
-  
-      const somaValores = this.itens.reduce((soma, item) => soma + getValorNumerico(item), 0);
-      return somaValores / this.itens.length;
-    }
+const app = express();
+const port =  8080;
+
+const pool = new Pool({
+  user: '***',
+  host: '****',
+  database: '****',
+  password: '*****',
+  port: ******,
+});
+
+app.use(bodyParser.json());
+app.use(express.static(path.join('index.html')));
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join('index.html'));
+});
+
+app.get('/pessoas', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query('SELECT * FROM usuarios');
+    const pessoas = result.rows;
+    res.json(pessoas);
+  } catch (error) {
+    console.error('Erro ao obter pessoas:', error);
+    res.status(500).json({ mensagem: 'Erro ao obter pessoas' });
   }
-  
-  const gerenciador = new Gerenciador<Pessoa<number>>();
-  
-  const pessoa1: Pessoa<number> = {
-    nome: 'Andre',
-    idade: 28,
-    endereco: {
-      rua: 'Alameda',
-      cidade: 'jacarei',
-    },
-  };
-  
-  const pessoa2: Pessoa<number> = {
-    nome: 'Adriano',
-    idade: 30,
-    endereco: {
-      rua: 'alameda',
-      cidade: 'SP',
-    },
-  };
-  
-  gerenciador.adicionarItem(pessoa1);
-  gerenciador.adicionarItem(pessoa2);
-  
-  console.log('Pessoas:', gerenciador.obterItens());
-  console.log('Média de Idade:', gerenciador.calcularMedia((pessoa) => pessoa.idade));
-  
-  gerenciador.removerItem((pessoa) => pessoa.nome === 'Adriano');
-  console.log('Pessoas após remoção:', gerenciador.obterItens());
-  console.log('Média de Idade após remoção:', gerenciador.calcularMedia((pessoa) => pessoa.idade));
+});
+
+app.post('/pessoas', async (req: Request, res: Response) => {
+  const { nome, email } = req.body;
+  try {
+    await pool.query('INSERT INTO usuarios (nome, email) VALUES ($1, $2)', [nome, email]);
+    const result = await pool.query('SELECT * FROM usuarios');
+    const pessoas = result.rows;
+    res.json(pessoas);
+  } catch (error) {
+    console.error('Erro ao adicionar pessoa:', error);
+    res.status(500).json({ mensagem: 'Erro ao adicionar pessoa' });
+  }
+});
+
+app.put('/pessoas/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { nome, email } = req.body;
+  try {
+    await pool.query('UPDATE usuarios SET nome = $1, email = $2 WHERE id = $3', [nome, email, id]);
+    const result = await pool.query('SELECT * FROM usuarios');
+    const pessoas = result.rows;
+    res.json(pessoas);
+  } catch (error) {
+    console.error('Erro ao atualizar pessoa:', error);
+    res.status(500).json({ mensagem: 'Erro ao atualizar pessoa' });
+  }
+});
+
+app.delete('/pessoas/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    await pool.query('DELETE FROM usuarios WHERE id = $1', [id]);
+    const result = await pool.query('SELECT * FROM usuarios');
+    const pessoas = result.rows;
+    res.json(pessoas);
+  } catch (error) {
+    console.error('Erro ao excluir pessoa:', error);
+    res.status(500).json({ mensagem: 'Erro ao excluir pessoa' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
